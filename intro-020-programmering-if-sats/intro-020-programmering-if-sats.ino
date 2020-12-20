32fc41afb7513a7218f15a6d7a76c7de3fee5e1b
Karlsson0214 <karlsson0214@gmail.com>
2020-12-20 18:56:31 +0100

    second

--- intro-020-programmering-if-sats/intro-020-programmering-if-sats.ino
+++ intro-020-programmering-if-sats/intro-020-programmering-if-sats.ino
@@ -4,6 +4,8 @@ void setup() {
   // överföringshastiheten är 9600 bitar,ettor eller nollor, per sekund
   Serial.begin(9600);
 
+  // för att det inte ska bli samma slumptal
+  // varje gång som programmet körs
   randomSeed(analogRead(A0));
 
   // variabel för att spara ett heltal
