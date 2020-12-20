f784900bcb8575b8f81654d559fd229e7d0111ed
Karlsson0214 <karlsson0214@gmail.com>
2020-12-20 18:47:07 +0100

    first

---
+++ intro-015-programmering-variabler/intro-015-programmering-variabler.ino
@@ -0,0 +1,36 @@
+
+
+void setup() {
+  Serial.begin(9600);
+
+  int x; 
+  x = 10;
+  int y = 20;
+  int sum = x + y;
+  Serial.print("x: ");
+  Serial.println(x); // utskrift: 10
+  Serial.print("y: ");
+  Serial.println(y); // 20
+  Serial.print("summa: ");
+  Serial.println(sum); // 30 
+  x = 2 * x;
+  Serial.print("x: ");
+  Serial.println(x); // 20
+  x = 10 * y;
+  Serial.print("x: ");
+  Serial.println(x); // 200
+  y = 50;
+  Serial.print("y: ");
+  Serial.println(y); // 50
+  Serial.print("x: ");
+  Serial.println(x); // 200
+
+  Serial.println("Arduino Uno");
+  String text = "Arduino Uno";
+  Serial.println(text); 
+
+}
+
+void loop() {
+
+}
