24f2741e9fe7bb3ed49ae44dcfd6fe5dab9ae28f
Karlsson0214 <karlsson0214@gmail.com>
2021-01-31 18:59:13 +0100

    demo array

---
+++ example-array/example-array.ino
@@ -0,0 +1,106 @@
+int n[ 4 ] ; // n is an array of 10 integers
+// a = .-
+int a[2];
+char message[10] = "hej";
+
+void setup () {
+  Serial.begin(9600);
+  Serial.println("--------------");
+
+  // ------------
+  // favorittal
+  // ------------
+  // variabel med plats för 5 heltal
+  // en variabel med plats för flera värden kallas vektor (eng. array)
+  int favorittal [5]; 
+  // har index 0, 1, 2, 3, 4
+  // index börjar alltid på 0 i C++
+  favorittal[0] = 2;
+  favorittal[1] = 3;
+  favorittal[2] = 5;
+  favorittal[3] = 7;
+  favorittal[4] = 11;
+
+  Serial.println("Mina favorittal: ");
+  int index = 0;
+  while (index < 5)
+  {
+    Serial.print(favorittal[index]);
+    Serial.print(", ");
+    index = index + 1;
+  }
+  Serial.println();
+
+  // ------------
+  // inköpslista
+  // ------------
+  String list [] = {"milk", "egg", "flour", "salt"};
+  Serial.println("--------------");
+  Serial.println("buy: ");
+  for (int i = 0; i < 4; i = i + 1)
+  {
+    Serial.println(list[i]);
+  }
+  Serial.println("--------------");
+
+  // ------------
+  // rövarspråk
+  // ------------
+  //char message [3] = {'h', 'e', 'j'};
+  // nedanstående rad ger samma vektor (eng. array) som raden ovan
+  char message [3] = "hej";
+  // nedanstående rad ger också samma resultat
+  //String message = "hej";
+  Serial.print(message);
+  Serial.println(" på rörvarspråk:");
+  // sizeof(message) ger värdet 3
+  // men för String blir det 6
+  // läs mer om sizeof(): https://www.arduino.cc/reference/en/language/variables/utilities/sizeof/
+  Serial.print("ordlängd: ");
+  Serial.println(sizeof(message));
+  for (int i = 0; i < sizeof(message); i = i + 1)
+  {
+    Serial.print(message[i]);
+    // om konsonant
+    // fuskar lite eftersom e är den enda vokalen i hej
+    if (message[i] != 'e')
+    {
+      Serial.print('o');
+      Serial.print(message[i]);      
+    }
+  }
+  Serial.println();
+  Serial.println("--------------");
+
+  // ------------
+  // Yatzy
+  // ------------
+  // vektor (eng. array) med plats för 5 heltal
+  int dice[5];
+  for (int i = 0; i < 5; i = i + 1)
+  {
+    // kasta 5 tärningar, en i taget
+    // spara resultatet i dice
+    dice[i] = random(1, 7);
+  }
+  Serial.println("Yatzy dice");
+  for (int i = 0; i < 5; i = i + 1)
+  {
+    Serial.print(dice[i]);
+    Serial.print(", ");
+  }
+  Serial.println();
+  int sum = 0;
+  for (int i = 0; i < 5; i = i + 1)
+  {
+    sum = sum + dice[i];
+  }
+  Serial.print("sum: ");
+  Serial.println(sum);
+  Serial.println("--------------");
+}
+
+void loop () 
+{
+
+}
